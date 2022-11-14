a2ddc87ff14c6a89a5bade6148862b096c3e0c49
JuniorAngeles <yuniorangeles06@gmail.com>
2022-11-13 20:04:17 -0400

    agregar styles y cambios en ruta main

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Libres from "./router/Libres";
 import Login from "./router/loginView";
-import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signout from "./router/singout";
 import Dasboard from "./router/Dasboar";
 import EditProfile from "./router/editProfile";
 import Profile from "./router/Profile";
 import UsernameView from "./router/UsernameView";
+import { CreateBook } from "./router/CreateBook";
 import { Navigate } from "react-router-dom";
+// import Header from "./components/Header";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    {/* <Header /> */}
     <Routes>
       <Route path="/" element={<Navigate to={"/Libres"} />} />
       <Route path="/Libres" element={<Libres />} />
@@ -22,6 +24,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Route path="/Libres/dashboard/profile" element={<EditProfile />} />
       <Route path="/Libres/u/:username" element={<Profile />} />
       <Route path="/Libres/choose-username" element={<UsernameView />} />
+      <Route path="/Libres/createBook" element={<CreateBook />} />
     </Routes>
   </BrowserRouter>
 );
