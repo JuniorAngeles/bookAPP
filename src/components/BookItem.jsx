e2ea8c27178c3a5e55f53829312cbca58b9fef3e
JuniorAngeles <yuniorangeles06@gmail.com>
2022-11-13 20:02:38 -0400

    agregar Header

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -1,11 +1,17 @@
+import "../styles/book.css";
+
 export function BookItem({ book }) {
   return (
-    <>
-      <h1>{book.nombre}</h1>
-      <p>{book.descriccion}</p>
-      <a target="_blank" href={book.url}>
-        <img src={book.img} alt={book.nombre} />
-      </a>
-    </>
+    <div className="books-group">
+      <div className="TD-book">
+        <h1>{book.nombre}</h1>
+        <p>{book.descriccion}</p>
+      </div>
+      <div className="img-book">
+        <a target="_blank" href={book.url}>
+          <img src={book.img} alt={book.nombre} />
+        </a>
+      </div>
+    </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import "../styles/header.css";
+
+export default function Header() {
+  return (
+    <header className="menu">
+      <h1 className="menu-title"> Cemas Library</h1>
+      <nav className="menu-nav">
+        <span className="menu-nav__link">
+          <Link className="menu-nav__link" to="/">
+            Inicio
+          </Link>
+        </span>
+        <span className="menu-nav__link">
+          <Link className="menu-nav__link" to="/Libres/createBook">
+            Subir Libro
+          </Link>
+        </span>
+      </nav>
+    </header>
+  );
+}
